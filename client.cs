3183f8566e801aa378d56539d7fdd40c0e0d1b37
atharvagangrade10 <44475067+atharvagangrade10@users.noreply.github.com>
2023-01-07 16:23:42 +0530

    Add project files.

---
+++ client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    public class client
+    {
+        public static Socket StartClient()
+        {
+            byte[] bytes = new byte[1024];
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress address = host.AddressList[0];
+                IPEndPoint endPoint = new IPEndPoint(address, 5000);
+
+                Socket sender = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    sender.Connect(endPoint);
+                    Console.WriteLine($"Socket Connected to {sender.RemoteEndPoint}");
+                    return sender;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    throw;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static string RecieveMessage(Socket handle)
+        {
+            string data = null;
+            byte[] bytes = new byte[1024];
+            int bytesRecive = handle.Receive(bytes);
+            data += Encoding.ASCII.GetString(bytes, 0, bytesRecive);
+            return data;
+        }
+        public static void SendMessage(Socket handle, string data)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            handle.Send(msg);
+        }
+        public static void CloseConnection(Socket handle)
+        {
+            handle.Shutdown(SocketShutdown.Both);
+            handle.Disconnect(true);
+            handle.Close();
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Tictactoe
+{
+    public class server
+    {
+        public static Socket StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress iPAddress = host.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, 5000);
+            try
+            {
+                Socket listner = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listner.Bind(endPoint);
+                listner.Listen(10);
+
+                Console.WriteLine("Waiting for Connection.....");
+                Socket handle = listner.Accept();
+                Console.WriteLine("Client is connected");
+                return handle;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+        public static string RecieveMessage(Socket handle)
+        {
+            string data = null;
+            byte[] bytes = new byte[1024];
+            int bytesRecive = handle.Receive(bytes);
+            data += Encoding.ASCII.GetString(bytes, 0, bytesRecive);
+            return data;
+        }
+        public static void SendMessage(Socket handle,string data)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            handle.Send(msg);
+        }
+        public static void CloseConnection(Socket handle)
+        {
+            handle.Shutdown(SocketShutdown.Both);
+            handle.Disconnect(true);
+            handle.Close();
+        }
+
+    }
+}
